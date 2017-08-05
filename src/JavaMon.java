fb15b19e0a0f70ae137cddf24207713e15ce0d92
Gary Curtis <curtis@stsci.edu>
2017-08-05 12:26:43 -0400

    Basic stub

--- src/JavaMon.java
+++ src/JavaMon.java
@@ -1,3 +1,6 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
 import javax.swing.*;
 
 /**
@@ -5,10 +8,31 @@ import javax.swing.*;
  */
 public class JavaMon extends JFrame {
 
+    private Action startAction = new AbstractAction("Start") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Starting...");
+        }
+    };
+    private Action exitAction = new AbstractAction("Exit") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    };
+    private JButton start = new JButton(startAction);
+    private JButton exit = new JButton(exitAction);
+    private Box buttons = Box.createHorizontalBox();
+
     public JavaMon () {
         setSize(500,500);
         setTitle("JavaMon");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        buttons.add(Box.createGlue());
+        buttons.add(start);
+        buttons.add(exit);
+        getContentPane().add(buttons, BorderLayout.SOUTH);
         setVisible(true);
     }
 
